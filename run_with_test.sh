867814d264fc6a61258bde4256c42b1bd615629f
Live session user <joshuaed@hawaii.edu>
2016-12-05 06:13:27 +0000

    use run_with_test.sh to pass in sample code

---
+++ run_with_test.sh
@@ -0,0 +1,1 @@
+bison -d foo.y && flex bar.l && g++-6 foo.tab.c lex.yy.c -ll -ly fubar.c && ./a.out < test.c && xdot fchart.dot
---
+++ test.c
@@ -0,0 +1,5 @@
+int main(){
+if(a < 2){a = 1+2;}
+if(a > 2){a = 3+3;}
+return 0;
+}
